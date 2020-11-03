980b9031da4b7a8297df4c817a6401b789ef1c84
DESKTOP-6PP8ISQ\TOSHIBA <jrdeleon@csmbataan.ph.education>
2020-11-03 17:31:33 +0800

    Adding label

---
+++ Daniel.Calling a Rest API/Daniel.Calling a Rest API/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daniel.Calling_a_Rest_API
+{
+    class Data
+    {
+    }
+}
